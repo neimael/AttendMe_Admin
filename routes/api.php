486dc50d773a861be9b3b1ee6bb0186067a1854e
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-04-27 22:26:23 +0000

    backend start

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\SanitaryIssuesController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//employees
+Route::get('employees', [EmployeeController::class, 'index']);
+//sanitaryIssues
+Route::post('add_sanitary_issue', [SanitaryIssuesController::class, 'store']);
+//admins
+Route::get('admins', [AdminController::class, 'index']);
+Route::post('add_admin', [AdminController::class, 'store']);
+
+
+
