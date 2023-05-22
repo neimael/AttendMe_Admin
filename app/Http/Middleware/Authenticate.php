5dcb67509024d0e6b974d2bccfd73adedb48fa01
Naima ELJID <114741266+neimael@users.noreply.github.com>
2023-05-22 18:10:22 +0100

    login sessions
    Co-Authored-By: Naima Adardor <98922186+naima-adardor@users.noreply.github.com>

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Http\Request;
+use Closure;
+use Illuminate\Support\Facades\Auth;
 
-class Authenticate extends Middleware
+class Authenticate
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     */
-    protected function redirectTo(Request $request): ?string
+    public function handle($request, Closure $next)
     {
-        return $request->expectsJson() ? null : route('login');
+        if (Auth::check()) {
+            return $next($request);
+        }
+
+        return redirect('/login'); // Redirect to the login page if not authenticated
     }
 }
